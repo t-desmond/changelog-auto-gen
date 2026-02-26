3b88c155fc12669ebc1291bb1f22c6cee8dfa31f
t-desmond <desmondtardzenyuy@gmail.com>
2026-02-26 15:46:41 +0100

    feat!: update operations logic

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,40 @@
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-fn subtract(a: i32, b: i32) -> i32 {
-    a - b
-}
-
-fn divide(a: i32, b: i32) -> i32 {
-    a / b
-}
-
 fn main() {
-    println!("{}", add(2, 2));
-    println!("{}", subtract(5, 3));
-    println!("{}", divide(6, 2));
+    println!("{}", MathOperations::add(2, 2));
+    println!("{}", MathOperations::subtract(5, 3));
+    println!("{}", MathOperations::divide(6, 2));
 }
 
+struct MathOperations;
+
+impl MathOperations {
+    fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+
+    fn subtract(a: i32, b: i32) -> i32 {
+        a - b
+    }
+
+    fn divide(a: i32, b: i32) -> i32 {
+        a / b
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_add() {
-        assert_eq!(add(2, 3), 5);
+        assert_eq!(MathOperations::add(2, 3), 5);
     }
 
     #[test]
     fn test_subtract() {
-        assert_eq!(subtract(5, 3), 2);
+        assert_eq!(MathOperations::subtract(5, 3), 2);
     }
 
     #[test]
     fn test_divide() {
-        assert_eq!(divide(6, 2), 3);
+        assert_eq!(MathOperations::divide(6, 2), 3);
     }
 }
