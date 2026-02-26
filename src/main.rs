b89245a54583cc882277ee7aebda7991a021a0d4
t-desmond <desmondtardzenyuy@gmail.com>
2026-02-26 13:06:48 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    println!("{}", add(2, 2));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+}
