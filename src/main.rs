0125867a453a46f85fa23391a770f9d7311e2fe8
t-desmond <desmondtardzenyuy@gmail.com>
2026-02-26 15:35:57 +0100

    feat: added support for subtraction subtration

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
+fn subtract(a: i32, b: i32) -> i32 {
+    a - b
+}
 fn main() {
     println!("{}", add(2, 2));
+    println!("{}", subtract(5, 3));
 }
 
 #[cfg(test)]
@@ -13,5 +17,10 @@ mod tests {
     #[test]
     fn test_add() {
         assert_eq!(add(2, 3), 5);
+    }
+
+    #[test]
+    fn test_subtract() {
+        assert_eq!(subtract(5, 3), 2);
     }
 }
